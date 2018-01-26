1f497a730db02b1bdaaa123f8d9d55760ab3780b
sudeepshop <35597206+sudeepshop@users.noreply.github.com>
2018-01-26 21:17:28 +0545

    Update home.client.controller.js

--- public/js/controllers/home.client.controller.js
+++ public/js/controllers/home.client.controller.js
@@ -1,9 +1,15 @@
 app.controller('HomeController', ['$scope','$http','toastr','User', 'Video', function($scope, $http, toastr, User, Video) {
-
+  
   $scope.listVideos =  function(){
     Video.retrieveAll().then(function(response){
-       $scope.allVideos = response.data;
-       console.log(response.data);
+       //$scope.allVideos = response.data;
+       //console.log(response.data);
+      $scope.allVideos = {
+        title: 'A Sample Video Title',
+        public_id: 'v1516980526',
+        url: 'http://res.cloudinary.com/sudguru/video/upload/v1516980526/003_-_Encapsulation_po3r59.mp4',
+        description: 'A sample test video'
+      }
     });
   };
 
